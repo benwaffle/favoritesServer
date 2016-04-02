27de6b2bd74526edd854a29bc3d36fd9ada5ed28
Ben Iofel <iofelben@gmail.com>
2016-04-02 10:33:29 -0400

    GET and POST; use es2015

--- index.js
+++ index.js
@@ -4,10 +4,21 @@ const express = require('express')
 
 const app = express()
 
-let count = {'CAL103': 3, 'CS181': 0 }
+let interested = {}
+
+app.post('/api/interested/:classid/:user', (req, res) => {
+    const { classid, user } = req.params
+    interested[classid] = interested[classid] || []
+    interested[classid].push(user)
+    res.send('ok')
+})
 
 app.get('/api/interested/:classid', (req, res) => {
-    res.json({ count: count[req.params.classid] })
+    const users = interested[req.params.classid]
+    if (users)
+        res.json({ count: users.length })
+    else
+        res.json({ count: 0 })
 })
 
 app.listen(3000, () => {
