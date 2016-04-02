7c29230c836868f2ad7d452773b71d9b6901bc53
Ben Iofel <iofelben@gmail.com>
2016-04-02 15:04:34 -0400

    enable cors

--- index.js
+++ index.js
@@ -8,6 +8,12 @@ const app = express()
 
 let interested = {}
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.get('/api/interested', (req, res) => {
     res.json(interested)
 })
